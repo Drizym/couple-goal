3f4cd78eb5817946d1290695e66da6d7bbfd2282
Rob <briclot.robin@gmail.com>
2024-07-06 16:06:20 +0200

    Initial Commit

---
+++ CoupleGoal/CoupleGoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleGoalApp.swift
+//  CoupleGoal
+//
+//  Created by Robin Briclot on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoupleGoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
